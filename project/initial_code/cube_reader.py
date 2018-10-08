76ddf94070cda16becc6da786bb687a5f52cd67b
Jacky Cao <probablyameow+github@gmail.com>
2018-10-08 22:06:07 +0100

    working with better gui interface

--- project/initial_code/cube_reader.py
+++ project/initial_code/cube_reader.py
@@ -2,16 +2,13 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 
-import sys
-from PyQt5.QtWidgets import QDialog, QApplication, QPushButton, QVBoxLayout
-
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
-import matplotlib.pyplot as plt
-
-import random
+from pyqtgraph.Qt import QtGui, QtCore
+import pyqtgraph as pg
 
 from astropy.io import fits
+
+#import pyqtgraph.examples
+#pyqtgraph.examples.run()
 
 def read_file(file_name):
     # reads file_name and returns specific header data and image data
@@ -70,81 +67,105 @@ def image_collapser(file_name):
             image_median[i_ra][i_dec]   = pd_median
             image_sum[i_ra][i_dec]      = pd_sum
 
-    plt.imshow(image_median, cmap='gray')
-    plt.colorbar()
-    plt.show()
+    #plt.imshow(image_median, cmap='gray')
+    #plt.colorbar()
+    #plt.show()
+
+    return image_median, image_sum
+
+#QtGui.QApplication.setGraphicsSystem('raster')
+app = QtGui.QApplication([])
+#mw = QtGui.QMainWindow()
+#mw.resize(800,800)
+
+win = pg.GraphicsWindow(title="Basic plotting examples")
+win.resize(1000,600)
+win.setWindowTitle('MUSE Basic 3D Cube Data')
+
+# Enable antialiasing for prettier plots
+pg.setConfigOptions(antialias=True)
+
+data_collapsed = image_collapser("cube_23.fits")
+
+p1 = win.addPlot(title="Collapsed: Median")
+im_med = pg.ImageItem()
+p1.addItem(im_med)
+
+im_med.setImage(data_collapsed[0])
 
 
-#initial = image_collapser("cube_23.fits")
-#print(initial[1])
+p2 = win.addPlot(title="Multiple curves")
+p2.plot(np.random.normal(size=100), pen=(255,0,0), name="Red curve")
+p2.plot(np.random.normal(size=110)+5, pen=(0,255,0), name="Green curve")
+p2.plot(np.random.normal(size=120)+10, pen=(0,0,255), name="Blue curve")
 
-class Window(QDialog):
-    def __init__(self, parent=None):
-        super(Window, self).__init__(parent)
+p3 = win.addPlot(title="Drawing with points")
+p3.plot(np.random.normal(size=100), pen=(200,200,200), symbolBrush=(255,0,0), symbolPen='w')
 
-        # a figure instance to plot on
-        self.figure = plt.figure()
 
-        # this is the Canvas Widget that displays the `figure`
-        # it takes the `figure` instance as a parameter to __init__
-        self.canvas = FigureCanvas(self.figure)
+win.nextRow()
 
-        # this is the Navigation widget
-        # it takes the Canvas widget and a parent
-        #self.toolbar = NavigationToolbar(self.canvas, self)
+p4 = win.addPlot(title="Parametric, grid enabled")
+x = np.cos(np.linspace(0, 2*np.pi, 1000))
+y = np.sin(np.linspace(0, 4*np.pi, 1000))
+p4.plot(x, y)
+p4.showGrid(x=True, y=True)
 
-        # Just some button connected to `plot` method
-        self.button = QPushButton('Plot')
-        self.button.clicked.connect(self.plot)
+p5 = win.addPlot(title="Scatter plot, axis labels, log scale")
+x = np.random.normal(size=1000) * 1e-5
+y = x*1000 + 0.005 * np.random.normal(size=1000)
+y -= y.min()-1.0
+mask = x > 1e-15
+x = x[mask]
+y = y[mask]
+p5.plot(x, y, pen=None, symbol='t', symbolPen=None, symbolSize=10, symbolBrush=(100, 100, 255, 50))
+p5.setLabel('left', "Y Axis", units='A')
+p5.setLabel('bottom', "Y Axis", units='s')
+p5.setLogMode(x=True, y=False)
 
-        # set the layout
-        layout = QVBoxLayout()
-        #layout.addWidget(self.toolbar)
-        layout.addWidget(self.canvas)
-        layout.addWidget(self.button)
-        self.setLayout(layout)
+p6 = win.addPlot(title="Updating plot")
+curve = p6.plot(pen='y')
+data = np.random.normal(size=(10,1000))
+ptr = 0
+def update():
+    global curve, data, ptr, p6
+    curve.setData(data[ptr%10])
+    if ptr == 0:
+        p6.enableAutoRange('xy', False)  ## stop auto-scaling after the first data set is plotted
+    ptr += 1
+timer = QtCore.QTimer()
+timer.timeout.connect(update)
+timer.start(50)
 
-    def plot(self):
-        file_name   = "cube_23.fits"
-        file_data   = read_file(file_name)
-        header_data = file_data[0]
-        image_data  = file_data[1]
 
-        data_shape  = np.shape(image_data)
-        ra_axis     = data_shape[2]
-        dec_axis    = data_shape[1]
-        wl_axis     = data_shape[0]
-        
-        image_median = np.zeros((ra_axis, dec_axis))
-        image_sum = np.zeros((ra_axis, dec_axis))
+win.nextRow()
 
-        for i_ra in range(ra_axis):
-            for i_dec in range(dec_axis):
-                pixel_data  = image_data[:][:,i_dec][:,i_ra]
-                pd_median   = np.median(pixel_data)
-                pd_sum      = np.sum(pixel_data)
+p7 = win.addPlot(title="Filled plot, axis disabled")
+y = np.sin(np.linspace(0, 10, 1000)) + np.random.normal(size=1000, scale=0.1)
+p7.plot(y, fillLevel=-0.3, brush=(50,50,200,100))
+p7.showAxis('bottom', False)
 
-                image_median[i_ra][i_dec]   = pd_median
-                image_sum[i_ra][i_dec]      = pd_sum
 
-        self.figure.clear()
+x2 = np.linspace(-100, 100, 1000)
+data2 = np.sin(x2) / x2
+p8 = win.addPlot(title="Region Selection")
+p8.plot(data2, pen=(255,255,255,200))
+lr = pg.LinearRegionItem([400,700])
+lr.setZValue(-10)
+p8.addItem(lr)
 
-        #image_median = np.flipud(image_median)
+p9 = win.addPlot(title="Zoom on selected region")
+p9.plot(data2)
+def updatePlot():
+    p9.setXRange(*lr.getRegion(), padding=0)
+def updateRegion():
+    lr.setRegion(p9.getViewBox().viewRange()[0])
+lr.sigRegionChanged.connect(updatePlot)
+p9.sigXRangeChanged.connect(updateRegion)
+updatePlot()
 
-        # create an axis
-        ax = self.figure.add_subplot(111)
-
-        # plot data
-        plt.imshow(image_median, cmap='gray')
-        plt.colorbar()
-
-        # refresh canvas
-        self.canvas.draw()
-
+## Start Qt event loop unless running in interactive mode or using pyside.
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-
-    main = Window()
-    main.show()
-
-    sys.exit(app.exec_())
+    import sys
+    if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
+        QtGui.QApplication.instance().exec_()
