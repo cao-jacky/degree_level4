9979a8d54d3d5c63b6db15a33a2f4c09745d264e
Jacky Cao <probablyameow+github@gmail.com>
2018-10-21 16:57:45 +0100

    l4: attempted to calculate chisq for redshift datas
    - plotted y=x for redshift data
    - attempted to calculate a chisq and reduced chisq for the redshift data
    -- values do not seem correct

--- project/code/cube_plots.py
+++ project/code/cube_plots.py
@@ -31,39 +31,49 @@ def data_matcher(cat_file_name, doublet_file):
     # running through data and finding required information
     usable_locs = np.where(doublet_regions[:,-1] == 1)[0]
     
-    # producing three columns: cube_id, model redshift, catalogue redshift
-    usable_data = np.zeros((len(usable_locs), 3))
+    # producing three columns: cube_id, catalogue redshift, model redshift, model
+    # redshift error
+    usable_data = np.zeros((len(usable_locs), 4))
     for i_cube in range(len(usable_locs)):
         cube_index = usable_locs[i_cube]
 
         cube_id = int(doublet_regions[cube_index][0])
         usable_data[i_cube][0] = cube_id
 
-        # looking at our generated model results
-        final_results_loc = ("results/cube_" + str(cube_id) + "/cube_" + str(cube_id) 
-            + "_fitting.txt")
-        final_results_file = open(final_results_loc)
-        
-        # 15th line in the final results file contains the optimal model redshift
-        curr_line = 0
-        for fr_line in final_results_file:
-            if (curr_line == 15):
-                rdst_line = fr_line.split()
-                rdst_val = rdst_line[-1]
-
-                usable_data[i_cube][1] = rdst_val
-            curr_line += 1
-
-        final_results_file.close()
-
         # looking at data from catalogue: col 44 has redshift data
         cat_cube_loc = np.where(catalogue_sorted[:,0] == cube_id)[0]
         cat_cube_data = catalogue_sorted[cat_cube_loc][0]
 
         cat_cube_rdst = cat_cube_data[43]
-        usable_data[i_cube][2] = cat_cube_rdst
+        usable_data[i_cube][1] = cat_cube_rdst
+
+        # looking at our generated model results
+        final_results_loc = ("results/cube_" + str(cube_id) + "/cube_" + str(cube_id) 
+            + "_lmfit.txt")
+        final_results_file = open(final_results_loc) 
+        
+        # 15th line in the final results file contains the optimal model redshift
+        curr_line = 0
+        for fr_line in final_results_file:
+            if (curr_line == 20):
+                rdst_line = fr_line.split()
+                rdst_val = rdst_line[1]
+                rdst_err = rdst_line[3]
+
+                usable_data[i_cube][2] = rdst_val
+                usable_data[i_cube][3] = rdst_err
+            curr_line += 1
+
+        final_results_file.close() 
 
     return usable_data
+
+def chisq(model, data, data_err):
+    csq = (data-model)**2 / data_err**2
+    csq_final = np.sum(csq)
+
+    redcsq = csq_final / (len(csq))
+    return {'chisq': csq_final, 'redchisq': redcsq}
 
 def plots(file_catalogue, file_doublets):
     data = data_matcher(file_catalogue, file_doublets)
@@ -72,25 +82,39 @@ def plots(file_catalogue, file_doublets):
     plt.rc('font', family='serif')
     plt.rcParams['text.latex.preamble'] = [r'\boldmath']
 
-    def redshift():
-        rdst_model = data[:,1]
-        rdst_cat = data[:,2]
+    analysis_file = open("results/analysis_results.txt", 'w')
 
+    def redshift():
+        rdst_model = data[:,2]
+        rdst_cat = data[:,1]
         cube_ids = data[:,0]
 
+        rdst_model_err = data[:,3]
+
+        chi_squared = chisq(rdst_cat, rdst_model, rdst_model_err)
+        analysis_file.write("Between the catalogue and model redshift data, we can " +
+            "calculate a chi-squared value of " + str(chi_squared['chisq']) + " and "
+            + " a reduced chi-squared value of " + str(chi_squared['redchisq']))
+
         fig, ax = plt.subplots()
+
+        x_min = np.min(rdst_model)
+        x_max = np.max(rdst_model)
+ 
+        x = np.linspace(x_min, x_max, 1000) 
+        ax.plot(x, x, color="#000000", alpha=0.3, linewidth=1)
+
         ax.scatter(rdst_model, rdst_cat, color="#000000", s=10)
 
         for i, txt in enumerate(cube_ids):
             ax.annotate(int(txt), (rdst_model[i], rdst_cat[i]))
 
-        ax.set_title(r'\textbf{Redshift Model vs. Catalogue}', fontsize=13) 
+        ax.set_title(r'\textbf{Redshift: Catalogue vs. Model}', fontsize=13) 
         ax.set_xlabel(r'\textbf{Model}', fontsize=13)
         ax.set_ylabel(r'\textbf{Catalogue}', fontsize=13)
         fig.savefig("graphs/redshift.pdf")
 
     redshift()
-
 
 file_catalogue  = "data/catalog.fits"
 file_doublets   = "data/cube_doublet_regions.txt"
