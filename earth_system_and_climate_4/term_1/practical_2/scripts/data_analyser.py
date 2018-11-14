f669398e62231fdfd667d17662db1d9a6f624975
Jacky Cao <probablyameow+github@gmail.com>
2018-11-14 20:21:49 +0000

    esc4: r: producing more graphs
    - redfit graphs
    - orbital data wavelet analysis graphs

--- earth_system_and_climate_4/term_1/practical_2/scripts/data_analyser.py
+++ earth_system_and_climate_4/term_1/practical_2/scripts/data_analyser.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy import genfromtxt
 
 import matplotlib
 import matplotlib.pyplot as plt
@@ -36,14 +37,14 @@ def plotter():
     plt.subplot(211)
     fd_x = foram_data[1:-1,0]
     fd_y = foram_data[1:-1,1]
-    plt.ylabel(r'\textbf{$\delta^{18}$O}', fontsize=13)
+    plt.ylabel(r'\textbf{$\delta^{18}$O}', fontsize=15)
     plt.xlim([0,1])
     plt.scatter(fd_x, fd_y, s=2.5, color="#000000")
     plt.plot(fd_x, fd_y, linewidth=0.5, color="#000000", alpha=0.2)
 
     plt.subplot(212)
-    plt.xlabel(r'\textbf{Age (Myr)}', fontsize=13)
-    plt.ylabel(r'\textbf{$\delta^{18}$O}', fontsize=13)
+    plt.xlabel(r'\textbf{Age (Myr)}', fontsize=15)
+    plt.ylabel(r'\textbf{$\delta^{18}$O}', fontsize=15)
     plt.xlim([4,5])
     plt.scatter(fd_x, fd_y, s=2.5, color="#000000")
     plt.plot(fd_x, fd_y, linewidth=0.5, color="#000000", alpha=0.2)
@@ -62,6 +63,7 @@ def plotter():
     #ax2.scatter(od_x, od_e, s=2.5, color="#f44336")
     #ax2.set_ylabel(r'\textbf{Eccentricity}', fontsize=13)
 
+    plt.tick_params(labelsize=15)
     plt.tight_layout()  # otherwise the right y-label is slightly clipped
     #plt.show()
     plt.savefig("foram_data.pdf", dpi=500)
@@ -87,7 +89,76 @@ def plotter():
     plt.ylabel(r'\textbf{Precessional index}', fontsize=13)
     plt.scatter(od_x, od_pi, s=5)
 
+    plt.close("all")
+
     #plt.show()
 
-plotter()
+def d18O_redfit():
+    data = genfromtxt('d-18O_redfit.csv', delimiter=',')
+
+    frequency = data[:,0]
+    power = data[:,1]
+    chisq95 = data[:,5]
+    
+    mask = np.where(power < chisq95)[0]
+    masked_frequency_regions = np.split(frequency, mask)
+    masked_power_regions = np.split(power, mask)
+
+    plt.figure()
+    plt.plot(frequency, power, linewidth=0.5, color="#000000")
+
+    for i in range(len(masked_frequency_regions)):
+        x_masked = masked_frequency_regions[i]
+        y_masked = masked_power_regions[i]
+        plt.plot(x_masked, y_masked, linewidth=0.5, color="#388e3c")
+
+    plt.plot(frequency, chisq95, linewidth=0.5, color="#e53935")
+
+    plt.xlabel(r'\textbf{Frequency (Myr$^{-1}$)}', fontsize=15)
+    plt.ylabel(r'\textbf{Power}', fontsize=15)
+    plt.tick_params(labelsize=15)
+
+    plt.xlim([0,60])
+    plt.tight_layout()  # otherwise the right y-label is slightly clipped
+    plt.savefig("d18O_redfit.pdf", dpi=500)
+    plt.close("all")
+
+    frequency_peaks = np.array([10.3882, 24.3173, 42.164])
+    wavelengths = 1/frequency_peaks
+
+    print(wavelengths)
+
+def heatmaps():
+    data = genfromtxt('wavelet_analysis_eccentricity.csv', delimiter=',')
+
+    log2_scale = data[1:,0]
+    x_scale = data[0:1,2:-1]
+
+    heatmap_data = data[1:,2:-1]
+    print(np.shape(heatmap_data))
+    print(heatmap_data)
+
+    plt.figure()
+    #plt.axis([np.min(x_scale), np.max(x_scale), np.min(log2_scale), 
+        #np.max(log2_scale)])
+    #plt.yticks(log2_scale)
+
+    plt.imshow(heatmap_data)
+
+    plt.xlabel(r'\textbf{Frequency (Myr$^{-1}$)}', fontsize=15)
+    plt.ylabel(r'\textbf{Power}', fontsize=15)
+
+    plt.tick_params(labelsize=15)
+    #plt.yticks(log2_scale)
+
+    plt.tight_layout()  # otherwise the right y-label is slightly clipped
+    plt.show()
+    plt.savefig("wa_eccentricity.pdf", dpi=500)
+    plt.close("all")
+
+
+
+#plotter()
+#d18O_redfit()
+heatmaps()
 
