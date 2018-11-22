4de48082ab083a30a7ba7f1cf1ae3c6731f88e43
Jacky Cao <probablyameow+github@gmail.com>
2018-11-22 15:39:47 +0000

    l4: manual noisy data

--- project/code/test_files/signal_noise.py
+++ project/code/test_files/signal_noise.py
@@ -11,16 +11,17 @@ def original_signal_generator():
     np.random.seed(0) # seeding the random numpy generator to create an original signal
 
     x_length = 1000
+    x_data = np.arange(0, x_length, 1)
 
     continuum_level = 100
-    noise = np.random.normal(continuum_level, 1, x_length)
-    
-    x_data = np.arange(0, x_length, 1)
-    y_data = noise
+    noise = np.random.normal(0, 1, x_length)
 
-    signal_to_noise = y_data / np.std(noise)
+    y_data = np.full(x_length, continuum_level)
+    y_data = y_data + noise
+
+    signal_to_noise = y_data / np.std(y_data)
     print(np.average(signal_to_noise))
-        
+ 
     plt.figure()
     plt.plot(x_data, y_data, linewidth=0.5, color="#000000")
 
@@ -28,7 +29,7 @@ def original_signal_generator():
     plt.ylabel(r'\textbf{y-axis}', fontsize=15)
 
     plt.tight_layout()
-    plt.savefig("signal_noise_graphs/"+str(continuum_level)+".pdf")
+    plt.savefig("signal_noise_graphs/signal_"+str(continuum_level)+".pdf")
     plt.close("all")
 
     return {'x': x_data, 'y': y_data}
@@ -51,13 +52,16 @@ def multiple_spectrums():
     spectrum = original_y
 
     for i in range(runs):
+        np.random.seed()
         new_noise = np.random.normal(0, 1, len(original_x))
+        print(np.std(spectrum))
         spectrum = spectrum + new_noise
 
         new_signal = spectrum
-        new_noise = np.std(spectrum)
+        new_noise_std = np.std(spectrum)
+        print(np.std(new_noise), new_noise_std)
 
-        new_sn = new_signal / new_noise
+        new_sn = new_signal / new_noise_std
         new_sn = np.average(new_sn)
 
         print(new_sn)
@@ -66,20 +70,32 @@ def multiple_spectrums():
         data[i][1] = new_sn
 
         plt.figure()
-        plt.plot(original_x, new_signal, linewidth=0.5, color="#000000")
+        plt.plot(original_x, new_noise, linewidth=0.5, color="#000000")
 
         plt.xlabel(r'\textbf{x-axis}', fontsize=15)
-        plt.ylabel(r'\textbf{y-axis}', fontsize=15)
+        plt.ylabel(r'\textbf{noise}', fontsize=15)
 
         plt.tight_layout()
-        plt.savefig("signal_noise_graphs/"+str(i)+".pdf")
+        plt.savefig("signal_noise_noise/noise_"+str(i)+".pdf")
+        plt.close("all")
+
+        plt.figure()
+        plt.plot(original_x, new_signal, linewidth=0.5, color="#b71c1c")
+        plt.plot(original_x, original_y, linewidth=0.5, color="#000000")
+
+
+        plt.xlabel(r'\textbf{x-axis}', fontsize=15)
+        plt.ylabel(r'\textbf{signal}', fontsize=15)
+
+        plt.tight_layout()
+        plt.savefig("signal_noise_graphs/signal_"+str(i)+".pdf")
         plt.close("all")
 
     plt.figure()
-    plt.plot(data[:,0], data[:,1], linewidth=0.5, color="#000000")
+    plt.plot(data[:,1], data[:,0], linewidth=0.5, color="#000000")
 
-    plt.xlabel(r'\textbf{x-axis}', fontsize=15)
-    plt.ylabel(r'\textbf{y-axis}', fontsize=15)
+    plt.xlabel(r'\textbf{signal to noise}', fontsize=15)
+    plt.ylabel(r'\textbf{index}', fontsize=15)
 
     plt.tight_layout()
     plt.savefig("signal_noise_results/index_vs_sn.pdf")
